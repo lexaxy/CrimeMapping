7fd5b9cb3ea23d984f623d9d80fb04686f774a7c
lexaxy <lexaxy@users.noreply.github.com>
2019-09-09 17:15:56 +0800

    Added Full Text Extractor
    Added a segment that visits all pages in the URL and extracts the full body text. Next steps are to tidy up and put into functions. Performance of program seems slow.

--- ST_Scraper.py
+++ ST_Scraper.py
@@ -1,11 +1,14 @@
 import bs4 as bs
 import urllib.request
 import time
+import pandas as pd
 
-maxPages = 117
+maxPages = 1
+
 urlList = list()
 headlineList = list()
 dateList = list()
+fullTextList = list()
 
 for i in range(maxPages):
     sauce = urllib.request.urlopen('https://www.straitstimes.com/singapore/courts-crime?page='+str(i))
@@ -15,7 +18,7 @@ for i in range(maxPages):
     dates = soup.body.find_all('div',{'class':'node-postdate'})
 
     for story in stories:
-        urlList.append(story.a.get('href'))
+        urlList.append('https://www.straitstimes.com'+ story.a.get('href'))
         headlineList.append(story.a.string)
 
     for date in dates:
@@ -23,3 +26,15 @@ for i in range(maxPages):
 
     time.sleep(1)
 
+for url in urlList:
+    sauce = urllib.request.urlopen(url)
+    soup = bs.BeautifulSoup(sauce, 'lxml')
+    articleBody = soup.body.find('div', {'itemprop': 'articleBody'})
+    text = articleBody.find_all('p')
+    textWOTags = ""
+    for i in text:
+        i = i.contents
+        textWOTags += (str(i[0]))
+        textWOTags += " "
+    fullTextList.append(textWOTags)
+    time.sleep(1)
