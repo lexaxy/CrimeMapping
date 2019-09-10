540fb5e64e501058c7a9c6a654a2cb2ae8195000
lexaxy <lexaxy@users.noreply.github.com>
2019-09-10 18:02:19 +0800

    Organized into functions and started area counter

--- ST_Scraper.py
+++ ST_Scraper.py
@@ -3,38 +3,74 @@ import urllib.request
 import time
 import pandas as pd
 
-maxPages = 1
+# global variables
+maxPages = 4
 
-urlList = list()
-headlineList = list()
-dateList = list()
-fullTextList = list()
+# function definitions
 
-for i in range(maxPages):
-    sauce = urllib.request.urlopen('https://www.straitstimes.com/singapore/courts-crime?page='+str(i))
-    soup = bs.BeautifulSoup(sauce,'lxml')
+def readContentsPages(maxpages):
+    # this function scrapes the contents pages of courts-crime articles on Straits Times and
+    # returns data frame of URLs, headlines, and dates of articles
 
-    stories = soup.body.find_all('span', {'class':'story-headline'})
-    dates = soup.body.find_all('div',{'class':'node-postdate'})
+    df = pd.DataFrame()
+    urlList = list()
+    headlineList = list()
+    dateList = list()
 
-    for story in stories:
-        urlList.append('https://www.straitstimes.com'+ story.a.get('href'))
-        headlineList.append(story.a.string)
+    for i in range(maxpages):
+        sauce = urllib.request.urlopen('https://www.straitstimes.com/singapore/courts-crime?page='+str(i))
+        soup = bs.BeautifulSoup(sauce,'lxml')
 
-    for date in dates:
-       dateList.append(date.string)
+        stories = soup.body.find_all('span', {'class':'story-headline'})
+        dates = soup.body.find_all('div',{'class':'node-postdate'})
 
-    time.sleep(1)
+        for story in stories:
+            urlList.append('https://www.straitstimes.com'+ story.a.get('href'))
+            headlineList.append(story.a.string)
 
-for url in urlList:
-    sauce = urllib.request.urlopen(url)
-    soup = bs.BeautifulSoup(sauce, 'lxml')
-    articleBody = soup.body.find('div', {'itemprop': 'articleBody'})
-    text = articleBody.find_all('p')
-    textWOTags = ""
-    for i in text:
-        i = i.contents
-        textWOTags += (str(i[0]))
-        textWOTags += " "
-    fullTextList.append(textWOTags)
-    time.sleep(1)
+        for date in dates:
+           dateList.append(date.string)
+
+        time.sleep(1)
+
+    df['url'] = urlList
+    df['headline'] = headlineList
+    df['date'] = dateList
+
+    return df
+
+def extractFullText(dataframe):
+    # this function takes as input the data frame generated from readContentsPages, and visits the article URL
+    # and adds another column with the full text of the article to the data frame
+    fullTextList = list()
+
+    for url in dataframe['url']:
+        sauce = urllib.request.urlopen(url)
+        soup = bs.BeautifulSoup(sauce, 'lxml')
+        articleBody = soup.body.find('div', {'itemprop': 'articleBody'})
+        text = articleBody.find_all('p')
+        textWOTags = ""
+        for i in text:
+            i = i.contents
+            textWOTags += (str(i[0]))
+            textWOTags += " "
+        fullTextList.append(textWOTags)
+        time.sleep(1)
+
+    dataframe['full Text'] = fullTextList
+
+    return dataframe
+
+
+df = readContentsPages(maxPages)
+df = extractFullText(df)
+
+stationNames = pd.read_csv('train-station-names.csv', usecols=["mrt_station_english"])
+areaCounter = dict.fromkeys(stationNames['mrt_station_english'],0)
+
+for text in df['full Text']:
+    for area in areaCounter.keys():
+        areaCounter[area] += text.count(area)
+
+##print(df)
+print(areaCounter)
