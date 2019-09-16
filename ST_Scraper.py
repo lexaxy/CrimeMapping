744a69731e7030a7083565741f1187193dd8da1a
lexaxy <lexaxy@users.noreply.github.com>
2019-09-17 00:33:32 +0800

    changed implementation of areaCounter
    areaCounter implementation takes only first occurrence of each area in the article text and does not double count.
    
    also added a counter to track progress of extractFullTextWithTitles

--- ST_Scraper.py
+++ ST_Scraper.py
@@ -3,10 +3,10 @@ import urllib.request
 import time
 import pandas as pd
 
-# global variables
-maxPages = 4
+## global variables
+maxPages = 114
 
-# function definitions
+## function definitions
 
 def readContentsPages(maxpages):
     # this function scrapes the contents pages of courts-crime articles on Straits Times and
@@ -43,36 +43,52 @@ def extractFullTextwithTitles(dataframe):
     # this function takes as input the data frame generated from readContentsPages, and visits the article URL
     # and adds another column with the full text of the article to the data frame
     fullTextList = list()
+    counter = 0
 
     for url in dataframe['url']:
-        sauce = urllib.request.urlopen(url)
-        soup = bs.BeautifulSoup(sauce, 'lxml')
-        articleBody = soup.body.find('div', {'itemprop': 'articleBody'})
-        text = articleBody.find_all('p')
-        textWOTags = ""
-        for i in text:
-            i = i.contents
-            textWOTags += (str(i[0]))
-            textWOTags += " "
-        fullTextList.append(textWOTags)
-        time.sleep(1)
+        try:
+            sauce = urllib.request.urlopen(url)
+            soup = bs.BeautifulSoup(sauce, 'lxml')
+            articleBody = soup.body.find('div', {'itemprop': 'articleBody'})
+            text = articleBody.find_all('p')
+            textWOTags = ""
+            for para in text:
+                sentence = para.contents
+                textWOTags += (str(sentence[0]))
+                textWOTags += " "
+            fullTextList.append(textWOTags)
+            time.sleep(1)
+            dataframe['full Text'] = fullTextList
+            dataframe['full Text'] = dataframe['headline'] + ' - ' + dataframe['full Text']
 
-    dataframe['full Text'] = fullTextList
-    dataframe['full Text'] = dataframe['headline'] + ' - ' + dataframe['full Text']
+        except:
+            print(url)
+            pass
+        counter += 1
+        if counter%10 == 0:
+            print(counter/10)
 
     return dataframe
 
+def runAreaCounter(dataframe):
+    # Checks the full texts from the articles for whether they contain mention of the area. The list
+    # of areas is obtained from the data.gov.sg MRT station names with duplicates removed and simplified.
+    # The full list of areas considered can be found in the .csv file
+    stationNames = pd.read_csv('train-station-names.csv', usecols=["station-names"])
+    areaCounter = dict.fromkeys(stationNames['station-names'], 0)
+
+    for text in dataframe['full Text']:
+        for area in areaCounter.keys():
+            if area in text:
+                areaCounter[area] += 1
+
+    print(areaCounter)
+
+## main body
 
 df = readContentsPages(maxPages)
 df = extractFullTextwithTitles(df)
+runAreaCounter(df)
 
-stationNames = pd.read_csv('train-station-names.csv', usecols=["station-names"])
-areaCounter = dict.fromkeys(stationNames['station-names'],0)
 
-for text in df['full Text']:
-    for area in areaCounter.keys():
-        if area in text:
-            areaCounter[area] += 1
-
-print(areaCounter)
 
